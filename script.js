f33aea0b06b626fdcb37870d4f94cb6fd22c8f8a
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-21 14:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let buttons = document.querySelectorAll(".btn");
+let stop = document.querySelector(".stop");
+let currentAudio = null;
+buttons.forEach((button) => {
+	button.addEventListener('click',function(){
+		if(currentAudio){
+			currentAudio.pause();
+			currentAudio.currentTime = 0;	
+		}
+
+		let currentSound = button.innerText;
+		currentAudio = new Audio(`sounds/${currentSound}.mp3`);
+		currentAudio.play();
+	});
+});
+
+stop.addEventListener('click',function(){
+	if(currentAudio){
+			currentAudio.pause();
+			currentAudio.currentTime = 0;	
+		}
+	
+});
